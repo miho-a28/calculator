9f6856b7077b9907015f43cf5e3471ab43f081f0
EC2 Default User <ec2-user@ip-172-31-42-129.ap-northeast-3.compute.internal>
2023-09-02 14:59:52 +0000

    質問-question

--- main.js
+++ main.js
@@ -1,17 +1,57 @@
+let btn= document.getElementById("hundred")
+
+
 function clickbutton(target){
     let displey=document.getElementById("displey")
     let target_value=target.innerHTML;
     
-    if(target_value=="AC"){
+    if(target_value=="AC"){        //AC押されたとき０と表示
         displey.innerHTML="0"
-    }else if(target_value=="="){
+        
+    }else if(target_value=="="){　　　　//=押されたとき結果を表示
         displey.innerHTML=eval(displey.innerHTML);
-    }else{
-        if( displey.innerHTML=="0"){
-             displey.innerHTML=target_value
-        }else{
-             displey.innerHTML+=target_value
-        }
-    } 
-}
     
+    
+ }else {                           //それ意外が押された時
+        
+        if(target_value == "+" && displey.innerHTML.slice(-1) === "+"){
+            return;
+        }else if(target_value == "+" && displey.innerHTML.slice(-1) === "-"){
+            return;
+        }else if(target_value == "+" && displey.innerHTML.slice(-1) === "*"){
+            return;
+        }else if(target_value == "+" && displey.innerHTML.slice(-1) === "/"){
+            return;
+            
+
+        }else if(target_value == "-" && displey.innerHTML.slice(-1) === "+"){
+            return;
+        }else if(target_value == "-" && displey.innerHTML.slice(-1) === "-"){
+            return;
+        }else if(target_value == "-" && displey.innerHTML.slice(-1) === "*"){
+            return;
+        }else if(target_value == "-" && displey.innerHTML.slice(-1) === "/"){
+            return;
+
+        }else if(target_value == "*" && displey.innerHTML.slice(-1) === "+"){
+            return;
+        }else if(target_value == "*" && displey.innerHTML.slice(-1) === "-"){
+            return;
+        }else if(target_value == "*" && displey.innerHTML.slice(-1) === "*"){
+            return;
+        }else if(target_value == "*" && displey.innerHTML.slice(-1) === "/"){
+            return;
+
+        }else if(target_value == "/" && displey.innerHTML.slice(-1) === "+"){
+            return;
+        }else if(target_value == "/" && displey.innerHTML.slice(-1) === "-"){
+            return;
+        }else if(target_value == "/" && displey.innerHTML.slice(-1) === "*"){
+            return;
+        }else if(target_value == "/" && displey.innerHTML.slice(-1) === "/"){
+            return;
+
+        }else{
+            displey.innerHTML = displey.innerHTML + target_value
+        }
+    }}
