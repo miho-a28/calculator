b4ec12964f3bdbc733d377aee0d3f7c0baa3f5c2
EC2 Default User <ec2-user@ip-172-31-42-129.ap-northeast-3.compute.internal>
2023-09-03 06:31:52 +0000

    再提出-resubmit

--- main.js
+++ main.js
@@ -1,20 +1,51 @@
-let btn= document.getElementById("hundred")
+ let displey=document.getElementById("displey")
+ 
+ 
+function clickbutton_clear(target){　　　   //AC押すと０表示
+    displey.innerHTML="0"
+}
 
 
-function clickbutton(target){
-    let displey=document.getElementById("displey")
-    let target_value=target.innerHTML;
+function clickbutton_result(target){     　//＝押すと結果表示
+　　displey.innerHTML=eval(displey.innerHTML);
+}
+
+function clickbutton_num(target){       　//０〜９の数字入力
+    let target_value=target.innerHTML
+    if(displey.innerHTML=="0"){
+        displey.innerHTML=target_value
+    }else{
+         displey.innerHTML+=target_value
+    }
+}
+
+function clickbutton_hum(target){         //00を押した時
+    let target_value=target.innerHTML
+    if(displey.innerHTML=="0"){          //もし０が入ってたら０を表示
+      displey.innerHTML="0"
+    }else{
+      displey.innerHTML+=target_value　　//もしそれ以外なら表示に＋00
+    }
+}
+
+function clickbutton_point(target){ 　    //小数点表示
+    let target_value=target.innerHTML
+    if(displey.innerHTML=="0"||"00"){
+        displey.innerHTML+=target_value
+    }
     
-    if(target_value=="AC"){        //AC押されたとき０と表示
+        
+}
+
+
+
+function clickbutton_sym(target){        //記号が押されたら     
+    let target_value=target.innerHTML
+    if(displey.innerHTML=="0"){　        //もし０が入ってたら０を表示
         displey.innerHTML="0"
-        
-    }else if(target_value=="="){　　　　//=押されたとき結果を表示
-        displey.innerHTML=eval(displey.innerHTML);
-    
-    
- }else {                           //それ意外が押された時
-        
-        if(target_value == "+" && displey.innerHTML.slice(-1) === "+"){
+    //}else if(displey.innerHTML.slice(-1)==target_value){　 //それ以外で記号が入ってたらそれ消して新しく入力
+    　　//return;
+        }else if(target_value == "+" && displey.innerHTML.slice(-1) === "+"){
             return;
         }else if(target_value == "+" && displey.innerHTML.slice(-1) === "-"){
             return;
@@ -22,8 +53,7 @@ function clickbutton(target){
             return;
         }else if(target_value == "+" && displey.innerHTML.slice(-1) === "/"){
             return;
-            
-
+        
         }else if(target_value == "-" && displey.innerHTML.slice(-1) === "+"){
             return;
         }else if(target_value == "-" && displey.innerHTML.slice(-1) === "-"){
@@ -50,8 +80,15 @@ function clickbutton(target){
             return;
         }else if(target_value == "/" && displey.innerHTML.slice(-1) === "/"){
             return;
-
-        }else{
-            displey.innerHTML = displey.innerHTML + target_value
-        }
-    }}
+        
+        
+    }else{
+       displey.innerHTML+=target_value 
+    }
+    
+    
+    
+    
+       
+ 
+}
